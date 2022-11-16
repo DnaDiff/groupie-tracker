f02a3edfaf524782bf7d6b7327b0453f0c5a9a05
Falusvampen <102164648+Falusvampen@users.noreply.github.com>
2022-11-16 17:26:32 +0200

    testing GetArtist functions

--- main.go
+++ main.go
@@ -1,7 +1,23 @@
 package main
 
+import (
+	"fmt"
+	get "groupie-tracker/server/Data"
+)
+
 func main() {
 
 	// server.StartServer()
+	// print the first artist in the list with newlines between each field
+	artists := get.GetArtists()
+	fmt.Println(artists[0].Id)
+	fmt.Println(artists[0].Image)
+	fmt.Println(artists[0].Name)
+	fmt.Println(artists[0].Members)
+	fmt.Println(artists[0].CreationDate)
+	fmt.Println(artists[0].FirstAlbum)
+	fmt.Println(artists[0].Locations)
+	fmt.Println(artists[0].ConcertDates)
+	fmt.Println(artists[0].Relations)
 
 }
