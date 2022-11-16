17faa76d3cb511041662b743edfbb38f9b05d06e
Falusvampen <102164648+Falusvampen@users.noreply.github.com>
2022-11-16 16:11:24 +0200

    created a startServer func

---
+++ server/server.go
@@ -0,0 +1,19 @@
+package server
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+var Port = "8080"
+
+func StartServer() {
+	http.Handle("/css/", http.StripPrefix("/css/", http.FileServer(http.Dir("css"))))
+	// http.HandleFunc("/", StartPage)
+	fmt.Println("Server started on port", Port)
+	err := http.ListenAndServe(":"+Port, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
