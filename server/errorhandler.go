f776946c7253c7d17c925332b0c14039e5694cfb
Falusvampen <102164648+Falusvampen@users.noreply.github.com>
2022-11-16 16:11:09 +0200

    imported old errorHandler.go

---
+++ server/errorhandler.go
@@ -0,0 +1,26 @@
+package server
+
+import (
+	"net/http"
+	"text/template"
+)
+
+type Error struct {
+	Code    int
+	Message string
+}
+
+// handles 500 errors
+func error500(w http.ResponseWriter, r *http.Request) {
+	d := Error{
+		Code:    http.StatusInternalServerError,
+		Message: "Internal Server Error",
+	}
+	errorHandler(w, r, &d)
+}
+
+func errorHandler(w http.ResponseWriter, r *http.Request, d *Error) {
+	tmpl = template.Must(template.ParseFiles("templates/error.html"))
+	w.WriteHeader(d.Code)
+	tmpl.ExecuteTemplate(w, "error.html", d)
+}
