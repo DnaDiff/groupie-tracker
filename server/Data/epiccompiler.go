eb76d288b097b7e99f44974e47269a62963c091a
Falusvampen <102164648+Falusvampen@users.noreply.github.com>
2022-11-23 14:18:30 +0200

    hello

--- server/Data/epiccompiler.go
+++ server/Data/epiccompiler.go
@@ -1,29 +1,34 @@
 package get
 
+type Everyone struct {
+	Id           int
+	Image        string
+	Name         string
+	Members      []string
+	CreationDate int
+	FirstAlbum   string
+	Locations    []string
+	ConcertDates []string
+	Relations    string
+}
 
-import (
-	"fmt"
-)
-
-func Epicmerge() {
-
-	// GetArtists()
-	// GetDates()
-	// GetLocations()
-
+func Epicmerge() []Everyone {
+	everyone := []Everyone{}
 	artists := GetArtists()
 	dates := GetDates()
 	locations := GetLocations()
-
-	fmt.Println(artists)
-	fmt.Println(dates)
-	fmt.Println(locations)
-
 	for i := 0; i < len(artists); i++ {
-		artists[i].ConcertDates = dates.Index[i].Dates
-		artists[i].Locations = locations.Index[i].Locations
+		everyone = append(everyone, Everyone{
+			Id:           artists[i].Id,
+			Image:        artists[i].Image,
+			Name:         artists[i].Name,
+			Members:      artists[i].Members,
+			CreationDate: artists[i].CreationDate,
+			FirstAlbum:   artists[i].FirstAlbum,
+			Locations:    locations.Index[i].Locations,
+			ConcertDates: dates.Index[i].Dates,
+			Relations:    artists[i].Relations,
+		})
 	}
-
-	fmt.Println(artists)
-
+	return everyone
 }
