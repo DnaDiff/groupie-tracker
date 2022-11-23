aae521f620f864192c8af7502c1d3a929cd6c9d6
Falusvampen <102164648+Falusvampen@users.noreply.github.com>
2022-11-23 14:17:31 +0200

    nice

--- main.go
+++ main.go
@@ -9,16 +9,26 @@ func main() {
 
 	// server.StartServer()
 	// print the first artist in the list with newlines between each field
-	nice := 2
+	nice := 13
 	artists := get.GetArtists()
+	dates := get.GetDates()
+	locations := get.GetLocations()
+
+	// // check if the length of the dates and locations slices are the same as the length of the artists slice
+	// fmt.Println(len(artists))
+	// fmt.Println(len(dates.Index))
+	// fmt.Println(len(locations.Index))
+
 	fmt.Println(artists[nice].Id)
 	fmt.Println(artists[nice].Image)
 	fmt.Println(artists[nice].Name)
 	fmt.Println(artists[nice].Members)
 	fmt.Println(artists[nice].CreationDate)
 	fmt.Println(artists[nice].FirstAlbum)
-	fmt.Println(artists[nice].Locations)
-	fmt.Println(artists[nice].ConcertDates)
+	fmt.Println(locations.Index[nice].Locations)
+	fmt.Println(dates.Index[nice].Dates)
 	fmt.Println(artists[nice].Relations)
 
+	// everyone := get.Epicmerge()
+	// fmt.Println(everyone[2])
 }
