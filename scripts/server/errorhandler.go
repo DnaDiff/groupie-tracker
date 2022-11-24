53b0aad34be0b606d8a7243b61a3689dacc380eb
Falusvampen <102164648+Falusvampen@users.noreply.github.com>
2022-11-25 01:10:25 +0200

    made error handlers

---
+++ scripts/server/errorhandler.go
@@ -0,0 +1,52 @@
+package server
+
+import (
+	"net/http"
+	"text/template"
+)
+
+type Error struct {
+	Code    int
+	Message string
+}
+
+var tmpl *template.Template
+
+// handles 500 errors
+func error500(w http.ResponseWriter, r *http.Request) {
+	d := Error{
+		Code:    http.StatusInternalServerError,
+		Message: "Internal Server Error",
+	}
+	errorHandler(w, r, &d)
+}
+
+func error404(w http.ResponseWriter, r *http.Request) {
+	d := Error{
+		Code:    http.StatusNotFound,
+		Message: "Page not found",
+	}
+	errorHandler(w, r, &d)
+}
+
+func error405(w http.ResponseWriter, r *http.Request) {
+	d := Error{
+		Code:    http.StatusMethodNotAllowed,
+		Message: "Method not allowed",
+	}
+	errorHandler(w, r, &d)
+}
+
+func error400(w http.ResponseWriter, r *http.Request) {
+	d := Error{
+		Code:    http.StatusBadRequest,
+		Message: "Bad request",
+	}
+	errorHandler(w, r, &d)
+}
+
+func errorHandler(w http.ResponseWriter, r *http.Request, d *Error) {
+	tmpl = template.Must(template.ParseFiles("templates/error.html"))
+	w.WriteHeader(d.Code)
+	tmpl.ExecuteTemplate(w, "error.html", d)
+}
