ff9855c21bcbeb363ff391fe0b326bab037fe41c
Falusvampen <102164648+Falusvampen@users.noreply.github.com>
2022-11-25 01:08:34 +0200

    testing

--- main.go
+++ main.go
@@ -1,33 +1,40 @@
 package main
 
-import (
-	"fmt"
-	get "groupie-tracker/server/Data"
-)
+import "groupie-tracker/scripts/server"
 
 func main() {
 
-	// server.StartServer()
+	server.StartServer()
 	// print the first artist in the list with newlines between each field
-	nice := 13
-	artists := get.GetArtists()
-	dates := get.GetDates()
-	locations := get.GetLocations()
+	// nice := 13
+	// artists := get.GetArtists()
+	// dates := get.GetDates()
+	// locations := get.GetLocations()
+	// nice := get.Epicmerge()
+	// fmt.Println(nice[51].Id)
+	// fmt.Println(nice[51].Image)
+	// fmt.Println(nice[51].Name)
+	// fmt.Println(nice[51].Members)
+	// fmt.Println(nice[51].CreationDate)
+	// fmt.Println(nice[51].FirstAlbum)
+	// fmt.Println(nice[51].Locations)
+	// fmt.Println(nice[51].ConcertDates)
+	// fmt.Println(nice[51].Relations)
 
 	// // check if the length of the dates and locations slices are the same as the length of the artists slice
 	// fmt.Println(len(artists))
 	// fmt.Println(len(dates.Index))
 	// fmt.Println(len(locations.Index))
 
-	fmt.Println(artists[nice].Id)
-	fmt.Println(artists[nice].Image)
-	fmt.Println(artists[nice].Name)
-	fmt.Println(artists[nice].Members)
-	fmt.Println(artists[nice].CreationDate)
-	fmt.Println(artists[nice].FirstAlbum)
-	fmt.Println(locations.Index[nice].Locations)
-	fmt.Println(dates.Index[nice].Dates)
-	fmt.Println(artists[nice].Relations)
+	// fmt.Println(artists[nice].Id)
+	// fmt.Println(artists[nice].Image)
+	// fmt.Println(artists[nice].Name)
+	// fmt.Println(artists[nice].Members)
+	// fmt.Println(artists[nice].CreationDate)
+	// fmt.Println(artists[nice].FirstAlbum)
+	// fmt.Println(locations.Index[nice].Locations)
+	// fmt.Println(dates.Index[nice].Dates)
+	// fmt.Println(artists[nice].Relations)
 
 	// everyone := get.Epicmerge()
 	// fmt.Println(everyone[2])
