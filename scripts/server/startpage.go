2352a4e70d9e6ed9331f83cb25f0e9eb0909c20b
Falusvampen <102164648+Falusvampen@users.noreply.github.com>
2022-11-29 01:59:53 +0200

    gave up on life but fixed it

--- scripts/server/startpage.go
+++ scripts/server/startpage.go
@@ -2,37 +2,31 @@ package server
 
 import (
 	get "groupie-tracker/scripts/Data"
+	"html/template"
 	"net/http"
-	"text/template"
 )
 
-// func StartPage(w http.ResponseWriter, r *http.Request) {
-
-// if r.URL.Path != "/" || r.Method != "GET" {
-// 	error404(w, r)
-// 	return
-// } else {
-// 	tmpl, err := template.ParseFiles("templates/index.html")
-// 	if err != nil {
-// 		error500(w, r)
-// 		return
-// 	}
-// 	err2 := tmpl.ExecuteTemplate(w, "index.html", get.Epicmerge())
-// 	if err2 != nil {
-// 		error500(w, r)
-// 		return
-// 	}
-// }
-// }
-
 func StartPage(w http.ResponseWriter, r *http.Request) {
-	nice := get.Epicmerge()
-	index, err := template.ParseFiles("templates/cocc.html")
-	if err != nil {
-		error500(w, r)
-		return
-	}
-	tmpl = template.Must(index, err)
 
-	tmpl.ExecuteTemplate(w, "index.html", nice)
+	Artists := get.Epicmerge()
+
+	switch {
+	case r.Method != "GET":
+		error404(w, r)
+	case r.URL.Path != "/":
+		error404(w, r)
+	default:
+
+		tmpl, err := template.ParseFiles("templates/index.html")
+		if err != nil {
+			error500(w, r)
+			return
+		}
+		err2 := tmpl.ExecuteTemplate(w, "index.html", Artists)
+		if err2 != nil {
+			error500(w, r)
+			return
+		}
+
+	}
 }
