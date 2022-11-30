c7e16758b3f5f3b975c9180ac9026ff9da0216dc
Falusvampen <102164648+Falusvampen@users.noreply.github.com>
2022-11-30 21:53:09 +0200

    switch and more functions

--- scripts/server/startpage.go
+++ scripts/server/startpage.go
@@ -4,20 +4,43 @@ import (
 	get "groupie-tracker/scripts/Data"
 	"html/template"
 	"net/http"
+	"strconv"
 )
+
+func CheckNumber(URL string) bool {
+	nbr, err := strconv.Atoi(URL)
+	if err != nil {
+		return false
+	}
+	if nbr > 0 && nbr < 53 {
+		return true
+	}
+	return false
+}
+
+func Atoi(s string) int {
+	nice, _ := strconv.Atoi(s)
+	return nice
+}
 
 func StartPage(w http.ResponseWriter, r *http.Request) {
 
 	Artists := get.Epicmerge()
+	// fmt.Println(Artists[Atoi(r.URL.Path[1:])].Id)
 
 	switch {
-	// case r.Method != "GET":
-	// 	error404(w, r)
-	// case r.URL.Path != "/":
-	// 	error404(w, r)
-	// case r.URL.Path != "/artists":
-	default:
-
+	case CheckNumber(r.URL.Path[1:]):
+		tmpl, err := template.ParseFiles("templates/artists.html")
+		if err != nil {
+			error500(w, r)
+			return
+		}
+		err2 := tmpl.ExecuteTemplate(w, "artists.html", Artists[Atoi(r.URL.Path[1:])-1])
+		if err2 != nil {
+			error500(w, r)
+			return
+		}
+	case r.URL.Path == "/":
 		tmpl, err := template.ParseFiles("templates/index.html")
 		if err != nil {
 			error500(w, r)
@@ -28,6 +51,55 @@ func StartPage(w http.ResponseWriter, r *http.Request) {
 			error500(w, r)
 			return
 		}
-
+	default:
+		error404(w, r)
+		return
 	}
+
+	// if CheckNumber(r.URL.Path[1:]) {
+	// 	tmpl, err := template.ParseFiles("templates/artists.html")
+	// 	if err != nil {
+	// 		error500(w, r)
+	// 		return
+	// 	}
+	// 	err2 := tmpl.ExecuteTemplate(w, "artists.html", Artists[Atoi(r.URL.Path[1:])-1])
+	// 	if err2 != nil {
+	// 		error500(w, r)
+	// 		return
+	// 	}
+	// } else if r.URL.Path == "/" {
+	// 	tmpl, err := template.ParseFiles("templates/index.html")
+	// 	if err != nil {
+	// 		error500(w, r)
+	// 		return
+	// 	}
+	// 	err2 := tmpl.ExecuteTemplate(w, "index.html", Artists)
+	// 	if err2 != nil {
+	// 		error500(w, r)
+	// 		return
+	// 	} else if r.URL.Path != "/" {
+	// 		error404(w, r)
+	// 	}
+	// }
+
+	// switch {
+	// // case r.Method != "GET":
+	// // 	error404(w, r)
+	// // case r.URL.Path != "/":
+	// // 	error404(w, r)
+	// // case r.URL.Path != "/artists":
+	// default:
+
+	// 	tmpl, err := template.ParseFiles("templates/index.html")
+	// 	if err != nil {
+	// 		error500(w, r)
+	// 		return
+	// 	}
+	// 	err2 := tmpl.ExecuteTemplate(w, "index.html", Artists)
+	// 	if err2 != nil {
+	// 		error500(w, r)
+	// 		return
+	// 	}
+
+	// }
 }
