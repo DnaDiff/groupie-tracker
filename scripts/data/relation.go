585ac1b75cd43688003874d31d514294dc25c2b7
Falusvampen <102164648+Falusvampen@users.noreply.github.com>
2022-11-30 21:52:14 +0200

    testing relations function

--- scripts/data/relation.go
+++ scripts/data/relation.go
@@ -1,6 +1,29 @@
 package get
 
-type Relations struct {
-	Id             uint64                 `json:"id"`
-	DatesLocations map[string]interface{} `json:"datesLocations"`
+import (
+	"encoding/json"
+	"log"
+	"strings"
+)
+
+type Relation struct {
+	Index []struct {
+		DatesLocations map[string][]string `json:"datesLocations"`
+	} `json:"index"`
+}
+
+// get the relations from the API
+func GetRelations() Relation {
+	data := GetData("https://groupietrackers.herokuapp.com/api/relations")
+	relations := Relation{}
+	// handle error if data is nil
+	err := json.Unmarshal(data, &relations)
+	if err != nil {
+		log.Panic("Problem in GetRelations function when unmarshalling data: ", err)
+		return Relation{}
+	}
+	test, _ := json.Marshal(relations.Index)
+	test = []byte(strings.Title(strings.ReplaceAll(string(test), "_", " ")))
+	json.Unmarshal(test, &relations.Index)
+	return relations
 }
