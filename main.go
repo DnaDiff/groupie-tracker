fc611e3bc619d75dcf65fed050340ae36a64223a
Kevazy <25287241+Kevazy@users.noreply.github.com>
2022-12-01 00:32:47 +0200

    Removed comments

--- main.go
+++ main.go
@@ -14,11 +14,5 @@ func main() {
 		return
 	}
 	server.StartServer()
-	// relate := get.GetRelations()
-	// fmt.Println(relate)
-	// print the first artist in the list with newlines between each field
-	// nice := 13
-	// fmt.Println(relate.Index[1].Id)
-	// fmt.Println(relate.Index[1].DatesLocations)
 
 }
