918035a59351f2fb711f951c2df641bc2c8acb9f
Kevazy <25287241+Kevazy@users.noreply.github.com>
2022-12-01 20:25:05 +0200

    Commented for later reference if needed

--- scripts/data/relation.go
+++ scripts/data/relation.go
@@ -1,29 +1,23 @@
 package data
 
-import (
-	"encoding/json"
-	"log"
-	"strings"
-)
+// import (
+// 	"encoding/json"
+// 	"log"
+// 	"strings"
+// )
 
-type Relation struct {
-	Index []struct {
-		DatesLocations map[string][]string `json:"datesLocations"`
-	} `json:"index"`
-}
-
-// get the relations from the API
-func GetRelations() Relation {
-	data := GetData("https://groupietrackers.herokuapp.com/api/relations")
-	relations := Relation{}
-	// handle error if data is nil
-	err := json.Unmarshal(data, &relations)
-	if err != nil {
-		log.Panic("Problem in GetRelations function when unmarshalling data: ", err)
-		return Relation{}
-	}
-	test, _ := json.Marshal(relations.Index)
-	test = []byte(strings.Title(strings.ReplaceAll(string(test), "_", " ")))
-	json.Unmarshal(test, &relations.Index)
-	return relations
-}
+// // get the relations from the API
+// func GetRelations() Relation {
+// 	data := getRawData("https://groupietrackers.herokuapp.com/api/relations")
+// 	relations := Relation{}
+// 	// handle error if data is nil
+// 	err := json.Unmarshal(data, &relations)
+// 	if err != nil {
+// 		log.Panic("Problem in GetRelations function when unmarshalling data: ", err)
+// 		return Relation{}
+// 	}
+// 	test, _ := json.Marshal(relations.Index)
+// 	test = []byte(strings.Title(strings.ReplaceAll(string(test), "_", " ")))
+// 	json.Unmarshal(test, &relations.Index)
+// 	return relations
+// }
