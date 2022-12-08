bc1e730932f8ed18d74eb44af643bb7393899fcd
Kevazy <25287241+Kevazy@users.noreply.github.com>
2022-12-08 16:29:48 +0200

    Typo fix

--- scripts/server/startpage.go
+++ scripts/server/startpage.go
@@ -36,13 +36,13 @@ func StartPage(w http.ResponseWriter, r *http.Request) {
 			httpError(w, r, http.StatusInternalServerError)
 			return
 		}
-	case r.URL.Path == "/useless":
-		tmpl, err := template.ParseFiles("templates/useless.html")
+	case r.URL.Path == "/useful":
+		tmpl, err := template.ParseFiles("templates/useful.html")
 		if err != nil {
 			httpError(w, r, http.StatusInternalServerError)
 			return
 		}
-		err = tmpl.ExecuteTemplate(w, "useless.html", Artists)
+		err = tmpl.ExecuteTemplate(w, "useful.html", Artists)
 		if err != nil {
 			httpError(w, r, http.StatusInternalServerError)
 			return
